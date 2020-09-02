8a6ac9c1e8a31b7454e00182cabe523c22789580
leejgdh <leejgdh@hotmail.com>
2020-09-02 23:21:39 +0900

    update validation form

--- KJMemo/Models/DTO/Account/PasswordResetViewModel.cs
+++ KJMemo/Models/DTO/Account/PasswordResetViewModel.cs
@@ -7,7 +7,7 @@ namespace KJMemo.Models.DTO{
 
 
         
-        [EmailAddress]
+        [EmailAddress(ErrorMessage="이메일 형식이 아닙니다.")]
         [DisplayName("이메일")]
         [Required(ErrorMessage = "{0}은 필수 입력사항 입니다.")]
         [StringLength(200, MinimumLength = 8, ErrorMessage = "{0}은 최소 8자 이상 되어야 합니다.")]
