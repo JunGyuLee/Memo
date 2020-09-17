ecca822fed259ddb63d5063b7460dd83984cf130
JunGyuLee <leejgdh@hotmail.com>
2020-09-17 16:29:19 +0900

    update migration

--- KJMemo/Migrations/20200917072837_update_note_model.Designer.cs
+++ KJMemo/Migrations/20200917072837_update_note_model.Designer.cs
@@ -4,14 +4,16 @@ using KJMemo.Models.DAO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KJMemo.Migrations
 {
     [DbContext(typeof(KJContext))]
-    partial class KJContextModelSnapshot : ModelSnapshot
+    [Migration("20200917072837_update_note_model")]
+    partial class update_note_model
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ KJMemo/Migrations/20200917072837_update_note_model.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KJMemo.Migrations
+{
+    public partial class update_note_model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Note",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(maxLength: 200, nullable: true),
+                    Content = table.Column<string>(maxLength: 2000, nullable: true),
+                    Category = table.Column<string>(maxLength: 1000, nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Note", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Note");
+        }
+    }
+}
